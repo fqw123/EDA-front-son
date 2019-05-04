3b0c015e05e477a3cf815abf96810630b35aff32
fqw <1693136836@qq.com>
2019-05-04 21:39:08 +0800

    git

--- src/component/action/editCode.js
+++ src/component/action/editCode.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import ReEditor from 're-editor';
 import {Link} from 'react-router-dom';
-import {Icon, Tabs, Input, Button,message} from 'antd';
+import {Icon, Tabs, Input,Table, Button,message,Card} from 'antd';
 import  './style.scss';
 import bind from 'react-autobind';
 import 're-editor/lib/styles/index.css';
-import axios from 'axios'
-
+import axios from 'axios';
 // https://github.com/wowlusitong/re-editor
 import Goback from '../../common/goBack';
+
 const Search = Input.Search;
 const TabPane = Tabs.TabPane;
+axios.defaults.headers['Content-Type'] = 'application/json; charset=utf-8';
 export default class view extends Component {
     constructor(props){
         super(props);
         this.state = {
+            // 管脚名
+            pinname:"",
+            pinvalue:'',
+            dataSource:[],
+            num:0,
+            // 实验结果
+            userinfo:"",
             editorValue: {},
             value: JSON.parse(localStorage.getItem('re-editor-value'))
         };
@@ -31,23 +39,156 @@ export default class view extends Component {
        
         if(!this.state.editorValue){
             message.destroy();
-            message.warning("请编辑内容")
+            message.warning("请编辑代码")
+        }else if(!this.state.dataSource.length){
+            message.destroy();
+            message.warning("请配置管脚")
         }else{
+            let pins={};
+            this.state.dataSource.filter(
+                (value,index)=>{
+                    pins[value.pinname]=value.pinvalue
+                }
+            )
             let params={
                 content:this.state.editorValue,
-                pins:''
+                pins:pins
             }
-            axios.post('http://localhost:3000/connect/code',params).then(
+            // console.log("params",params)
+            let timer;
+            clearInterval(timer);
+            axios.post('http://192.168.1.103:3000/connect/code',params).then(
                 rem=>{
                     console.log("提交",rem)
+                    if(rem.data.code===200){
+                        message.config({
+                            top: 100,
+                            duration: 8,
+                          
+                          });
+                          message.info("正在编译，请稍候")
+                           let num=0;
+                            timer=setInterval(
+                               ()=>{
+                                num++;
+                                if(num===4){
+                                    clearInterval(timer);
+                                    console.log("三次啦",num)
+                                }else{
+                                    this.getResult(timer)
+
+                                    console.log("调用getResult",num)
+                                }
+                               },10000
+                           )
+                    }else{
+                        message.info("sorry,服务器开小差了！")
+                    }
                 }
             )
         }
        
     }
+ 
+
+    getResult(timer){
+        axios.post('http://192.168.1.103:3000/connect/getstatus').then(
+            rem=>{
+                console.log("请求结果",rem)
+                if(rem.data.code===200){
+                    clearInterval(timer);
+                    this.setState({
+                        userinfo:rem.data.info
+                    })
+                    message.destroy();
+                    message.success("编译成功")
+                }else{
+                    
+                    message.destroy();
+                    message.success("串口连接失败，请重试")
+                    this.setState({
+                        userinfo:""
+                    })
+                }
+            }
+        )
+    }
+    pinname(e){
+        e.persist();
+        this.setState({
+            pinname:e.target.value  
+        })
+    }
+    pinvalue(e){
+        e.persist();
+        let reg = /^[0-9a-zA-Z]+$/;
+        let str=e.target.value;
+        if(!reg.test(str)&&str.length>0){
+            message.destroy();
+             message.error("管脚值只能输入字母或数字")
+        }else{
+            this.setState({
+                pinvalue:e.target.value  
+            })
+        }
+       
+    }
+    addpins(){
+        if(!this.state.pinname){
+            message.destroy();
+            message.warning("请输入管脚名")
+           
+        }else if(!this.state.pinvalue){
+           
+                        message.destroy();
+                        message.warning("请输入管脚值")
+           
+        }else{
+
+        let bool= (k)=>{
+            let ft=true;
+            let da=this.state.dataSource
+            console.log("比较",da,k)
+            da.filter(
+                (value,index)=>{
+                    console.log("filter",value,index)
+                    if(value.pinname===k){
+                        ft=false
+                    }else{
+                        ft=true
+                    }
+                }
+            )
+            return ft;
+          
+          }
+        
+            if(bool(this.state.pinname)){
+
+                this.state.dataSource.push({
+                key:this.state.pinname,
+                pinname:this.state.pinname,
+                pinvalue:this.state.pinvalue
+               })
+                console.log("添加",this.state.dataSource,this.state.num)
+                this.setState({
+                    num:Number(this.state.num)+1
+                })
+              
+            }else{
+                message.error("管脚名已存在,请修改")
+            }
+
+
+
+
+        }
+    }
+  
+
 
     render() {
-        const { value } = this.state;
+        const { value,userinfo ,pinname,pinvalue} = this.state;
         return (
             <div className="container">
                 <h1>QuartesII代码编辑</h1>
@@ -57,10 +198,65 @@ export default class view extends Component {
                     placeholder="请输入内容"
                     
                 />
-                <div  className="tijiao">  <Button type="primary" onClick={this.submit}    size={"large"}>提交</Button></div>
+              
+                <div  className="peihi">
+                    <h2>配置pins管脚</h2>
+                    <p  className="tablePins"><span  className="piname">管脚名:</span>  <Input  value={pinname} onChange={this.pinname}   />  <span  className="piname">管脚值:</span><Input  value={pinvalue} onChange={this.pinvalue}   /> <p  className="piname"><Button  type="primary"  onClick={this.addpins}>添加</Button></p> </p>
+                    
+                     <Table   dataSource={this.state.dataSource }   pagination={false}   num={this.state.num}   columns={this.columns}   />
+                </div>
+
+                <div  className="tijiao">  <Button type="primary" onClick={this.submit}    size={"large"}>提交</Button> </div>
+                                
+                <div  className="card" > 
+                    <Card
+                    size="small"
+                    title={'编译结果'}
+                    style={{ width: 400 }}
+                    >
+
+                    {userinfo?
+                    <p>{userinfo}</p>:
+
+                    <p  style={{minHeight:'200px'}}>暂无数据</p>
+                    }
+
+                    </Card>
+                
+                </div>
             </div>
 
         );
     }
+    deletel(record,index,){
+        console.log("index,record",index,record,this.state.dataSource)
+        let k=[...this.state.dataSource]  
+        k.splice(index,1);
+        this.setState({
+            dataSource:k,
+            num:Number(this.state.num)+1
+        })
+        
+       
+    }
+    columns=[
+        {
+          key:'keyname',
+          dataIndex:'pinname',
+          title:'管脚名称'
+        },{
+            key:'keyvalue',
+            dataIndex:'pinvalue',
+            title:'管脚值'
+          }
+          ,{
+            key:'action',
+            dataIndex:'action',
+            title:'操作',
+            render:(text,record,index)=>{
+                return <span onClick={this.deletel.bind(this,record,index)}   style={{color:'#1890ff'}}>删除</span>
+            }
+          },
+    ]
 }
 
--- src/component/action/vision1.js
+++ src/component/action/vision1.js
@@ -3,16 +3,20 @@ import bind from 'react-autobind'
 import {Link} from 'react-router-dom';
 import {Icon, Tabs, Input, Button,message} from 'antd';
 import   './style.scss';
+import Axios from 'axios';
 
 export default class index extends Component {
  constructor(props){
    super(props);
    this.state={
       pinlv:'',
-      dianya:""
+      dianya:"",
+      statusModal:'',
+      switchKey:''
    }
    bind(this);
  }
+//  频率 （8字节string）
  pinlv(v){
    v.persist();
    console.log()
@@ -22,12 +26,22 @@ export default class index extends Component {
      message.error("只能输入纯数字")
 
    }else{
-    this.setState({
-      pinlv:v.target.value
-     })
+    if(v.target.value.length>8){
+      this.setState({
+        pinlv:v.target.value.substring(0,8)
+        })
+    }else{
+      this.setState({
+        pinlv:v.target.value
+        })
+    }
+   
    }
   
  }
+
+
+ //  模拟电压（3字节string）
  dianya(v){
   v.persist();
   console.log()
@@ -37,19 +51,111 @@ export default class index extends Component {
     message.error("只能输入纯数字")
 
   }else{
-    
-   this.setState({
-    dianya:v.target.value
-    })
+  
+
+    if(v.target.value.length>3){
+      this.setState({
+        dianya:v.target.value.substring(0,3)
+        })
+    }else{
+      this.setState({
+        dianya:v.target.value
+        })
+    }
   }
  }
-   
 
+//  状态模式 1字节string
+statusModal(v){
+  v.persist();
+  console.log()
+  var reg=/^[0-9]*$/;
+  if(!reg.test(v.target.value)){
+    message.destroy();
+    message.error("只能输入纯数字")
+
+  }else{
+    if(v.target.value.length>1){
+      this.setState({
+        statusModal:v.target.value.substring(0,1)
+        })
+    }else{
+      this.setState({
+        statusModal:v.target.value
+        })
+    }
+  }
+}
+
+  //  按键开关 2字节string
+ switchKey(v){
+  v.persist();
+  console.log()
+  var reg=/^[0-9]*$/;
+  if(!reg.test(v.target.value)){
+    message.destroy();
+    message.error("只能输入纯数字")
+
+  }else{
+    if(v.target.value.length>2){
+      this.setState({
+        switchKey:v.target.value.substring(0,2)
+        })
+    }else{
+      this.setState({
+        switchKey:v.target.value
+        })
+    }
+  
+  }
+ }
+ tijiao(){
+   console.log("switchKey",this.state.switchKey,!this.state.pinlv.length&&this.state.switchKey.length&&this.state.statusModal.length&&this.state.dianya.length)
+   if(!this.state.pinlv.length&&this.state.switchKey.length&&this.state.statusModal.length&&this.state.dianya.length){
+     message.destroy();
+     message.warning("每一项都必填哦")
+   }else{
+            let params={
+              ctrldata:{
+                'frequency':this.dodata(this.state.pinlv,8),
+                'switch':this.dodata(this.state.switchKey,2),
+                'mode':this.state.statusModal,
+                'analog':this.dodata(this.state.dianya,3)
+              }
+
+        }
+        console.log("rem",params)
+        Axios.post('http://http://192.168.1.103:3000/connect/ctrl',params).then(
+        rem=>{
+          console.log("rem",rem)
+        }
+        )
+   }
+   
+ }
+ dodata(str,length){
+  let string;
+  if(str.length<length){
+    let arr=str.split("");
+    while(arr.length<length){
+      arr.unshift('0')
+    }
+    arr= arr.join("");
+    console.log("arr",arr)
+     return arr;
+    
+   }else{
+    return str.substring(0,length)
+   }
+}
   render() {
     return (
       <div  className="vision1">
           <div    className="pline"><span  className="ptxt"  >频 率 值:</span><div  className={"pint"}><Input onChange={this.pinlv}  value={this.state.pinlv}   /></div></div>
           <div    className="pline"><span   className="ptxt">模拟电压:</span><div   className={"pint"}><Input onChange={this.dianya}  value={this.state.dianya}   /></div></div>
+          <div    className="pline"><span  className="ptxt"  >状态模式:</span><div  className={"pint"}><Input onChange={this.statusModal}  value={this.state.statusModal}   /></div></div>
+          <div    className="pline"><span   className="ptxt">按键开关:</span><div   className={"pint"}><Input onChange={this.switchKey}  value={this.state.switchKey}   /></div></div>
+          <Button  onClick={this.tijiao}  >提交</Button>
       </div>
     )
   }
